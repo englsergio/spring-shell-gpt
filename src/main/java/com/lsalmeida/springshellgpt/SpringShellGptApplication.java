859e4ab02058c24aed7f63e936ed2e834cc920b2
Luís Sérgio Almeida <eng.lsergio@gmail.com>
2023-05-17 20:43:14 -0300

    Commit inicial

---
+++ src/main/java/com/lsalmeida/springshellgpt/SpringShellGptApplication.java
@@ -0,0 +1,13 @@
+package com.lsalmeida.springshellgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShellGptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringShellGptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lsalmeida/springshellgpt/SpringShellGptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsalmeida.springshellgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShellGptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
